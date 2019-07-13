6fcb74908720282b3340bdd3c5c7d0302b518667
Christopher Goh <chrisgzf@gmail.com>
2019-07-14 01:36:10 +0800

    Initial commit. Prototyped the functions.

---
+++ bot_secrets_SAMPLE.py
@@ -0,0 +1,15 @@
+"""
+This file is used to store your bot's token.
+
+Using your telegram app, speak to @BotFather, and create a new bot.
+
+Afterwards, @BotFather will give you a bot token, which you will then
+insert below. Please rename this file as `bot_secrets.py` so that it
+will be successfully imported
+
+After you add the token to this file, please make sure you do not
+commit the file to any public repos, or other's can use your bot token
+and possibly do nefarious things.
+"""
+
+TOKEN = "YOUR-BOT-TOKEN-HERE"
